7ec3dde59c5a76150bd6056d34987c590929fba0
dev-px <152850638+dev-px@users.noreply.github.com>
2023-12-15 02:04:14 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,42 +8,49 @@ const IV_LENGTH = parseInt(process.env.IV_LENGTH);
 
 
 //signup or register route
-exports.userSignup = (req, res) => {
-  console.log(req.body);
-  let iv = crypto.randomBytes(IV_LENGTH);
-  let cipher = crypto.createCipheriv(
-    algorithm,
-    Buffer.from(ENCRYPTION_KEY, "hex"),
-    iv
-  );
-  let encrypted = cipher.update(req.body.password);
-  encrypted = Buffer.concat([encrypted, cipher.final()]);
-  const encrypt = iv.toString("hex") + ":" + encrypted.toString("hex");
-  console.log(encrypt);
+exports.userSignup = async (req, res) => {
+  try {
+    console.log(req.body);
+    let iv = crypto.randomBytes(IV_LENGTH);
+    let cipher = crypto.createCipheriv(
+      algorithm,
+      Buffer.from(ENCRYPTION_KEY, "hex"),
+      iv
+    );
+    let encrypted = cipher.update(req.body.password);
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    const encrypt = iv.toString("hex") + ":" + encrypted.toString("hex");
+    console.log(encrypt);
 
-  const data = new User({
-    name: req.body.name,
-    email: req.body.email,
-    address: req.body.address,
-    password: encrypt,
-  });
-
-  data.save()
-    .then((result) => {
-      jwt.sign({ result }, process.env.JWTKEY,{ expiresIn: "500s" }, (err, token) => {
-          res.status(201).json(token);
-        }
-      );
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: "Internal Server Error" });
+    const data = new User({
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address,
+      password: encrypt,
     });
+
+    const result = await data.save();
+
+    const token = jwt.sign({ result }, process.env.JWTKEY, {
+      expiresIn: "600s",
+    });
+
+    res.status(201).json(token);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 // login route
-exports.userLogin = (req, res) => {
-  User.findOne({ email: req.body.email }).then((data) => {
+exports.userLogin = async (req, res) => {
+  try {
+    const data = await User.findOne({ email: req.body.email });
+
+    if (!data) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
     let textParts = data.password.split(":");
     let iv = Buffer.from(textParts.shift(), "hex");
     let encryptedText = Buffer.from(textParts.join(":"), "hex");
@@ -55,12 +62,17 @@ exports.userLogin = (req, res) => {
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     const decrypt = decrypted.toString();
-    console.log(decrypt);
 
-    if (decrypt == req.body.password) {
-      jwt.sign({ data }, process.env.JWTKEY, { expiresIn: "500s" }, (err, token) => {
-          res.status(200).json(token);
-        });
+    if (decrypt === req.body.password) {
+      const token = jwt.sign({ data }, process.env.JWTKEY, {
+        expiresIn: "600s",
+      });
+      res.status(200).json(token);
+    } else {
+      res.status(401).json({ error: "Invalid credentials" });
     }
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
